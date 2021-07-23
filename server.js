2bc5d5e13f5c4eccc692d2a6422bcac5f88d79b1
Michael Douglas <michael.dcsouza@gmail.com>
2021-07-23 12:31:02 -0300

    - Added MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const server = require('express')();
 const app = require('./config/express')();
+const config = require('config');
+const mongoose = require('mongoose');
 const port = app.get('port');
-const baseURL = '/v1'
 
-server.use(baseURL, app).listen(port, () => {
-  console.log(`Server is Listening on Port ${port}`)
+// MONGODB CONNECTION
+const databaseURI = process.env.MONGODB_URI || config.database.uri
+
+const mongodbOptions = { 
+  useUnifiedTopology: true,
+  useNewUrlParser: true 
+};
+
+mongoose.connect(databaseURI, mongodbOptions).then(
+  () => { console.log('✅ MongoDB Connected Successfully!') },
+  err => { console.log(`❌ MongoDB Failed to Connect Error ${err}`) }
+)
+
+// SERVER LISTENING ON CONFIGURED PORT
+server.use('/v1', app).listen(port, () => {
+  console.log(`🚀 Server is Listening on Port ${port}`)
 });
