3c93b8e51c29d09fb77e81f7f813f3019d23ecb8
Michael Douglas <michael.dcsouza@gmail.com>
2021-07-28 02:22:25 -0300

    - Small test

--- config/passport.js
+++ config/passport.js
@@ -12,12 +12,9 @@ const options = {
 // CONFIGURE JWT STRATEGY
 const strategy = new JWTStrategy(options, async (token, done) => {
     try {
-        console.log('DEU BOM')
         return done(null, token);
     } catch (error) {
-        console.log('DEU MERDA');
-        console.log(process.env.TOKEN_SECRET);
-        done(error);
+        done(`DEU MERDA ${process.env.TOKEN_SECRET}`);
     }
 });
 
